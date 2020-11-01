7acbcab98398e209be103ba0bf56fee9d76bec48
Michael Lage <lage.michael@gmail.com>
2020-11-01 15:04:36 -0700

    added Botmation link, added notes, removed ui-first-dev post
    - great tutorial on egghead summarizes content easily

--- src/gatsby-theme-blog/components/header.js
+++ src/gatsby-theme-blog/components/header.js
@@ -3,24 +3,48 @@ import { Link } from "gatsby"
 import { css, Styled } from "theme-ui"
 import Header from "gatsby-theme-blog/src/components/header"
 
+import styled from "@emotion/styled"
+
+const NavigationLinks = styled.div`
+  flex: 1;
+`
+
 export default props => (
   <Header {...props}>
-    <Styled.a
-      as={Link}
-      to="/notes"
-      css={css({
-        ml: 2,
-        mr: `auto`,
-        fontFamily: `heading`,
-        fontWeight: `bold`,
-        textDecoration: `none`,
-        color: `inherit`,
-        ":hover": {
-          textDecoration: `underline`,
-        },
-      })}
-    >
-      Notes
-    </Styled.a>
+    <NavigationLinks>
+      <Styled.a
+        href="https://botmation.dev"
+        css={css({
+          ml: 2,
+          mr: `auto`,
+          fontFamily: `heading`,
+          fontWeight: `bold`,
+          textDecoration: `none`,
+          color: `inherit`,
+          ":hover": {
+            textDecoration: `underline`,
+          },
+        })}
+      >
+        Botmation
+      </Styled.a>
+      <Styled.a
+        as={Link}
+        to="/notes"
+        css={css({
+          ml: 2,
+          mr: `auto`,
+          fontFamily: `heading`,
+          fontWeight: `bold`,
+          textDecoration: `none`,
+          color: `inherit`,
+          ":hover": {
+            textDecoration: `underline`,
+          },
+        })}
+      >
+        Notes
+      </Styled.a>
+    </NavigationLinks>
   </Header>
 )
