43d53c87a91938cb5a1c9668e074ca76e8df4117
Michael Lage <lage.michael@gmail.com>
2024-03-19 21:48:12 -0600

    social links reordered

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -9,6 +9,6 @@ export default () => (
   <Fragment>
     Written by Michael Lage
     <br />
-    <Styled.a href="http://twitter.com/mpbl" target="_blank">Twitter</Styled.a>, <Styled.a href="http://github.com/mrWh1te" target="_blank">Github</Styled.a>, <Styled.a href="http://linkedin.com/in/michaellage" target="_blank">LinkedIn</Styled.a>, <Styled.a href="http://instagram.com/lagmahol" target="_blank">Instagram</Styled.a>
+    <Styled.a href="http://instagram.com/lagmahol" target="_blank">Instagram</Styled.a>, <Styled.a href="http://twitter.com/mpbl" target="_blank">Twitter</Styled.a>, <Styled.a href="http://github.com/mrWh1te" target="_blank">Github</Styled.a>, <Styled.a href="http://linkedin.com/in/michaellage" target="_blank">LinkedIn</Styled.a>
   </Fragment>
 )
